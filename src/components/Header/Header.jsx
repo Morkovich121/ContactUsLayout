f71f9351a5c0430994fba16a4e96f83ce41729cb
DESKTOP-IOQP7MN\User <ilyasereda121@gmail.com>
2023-03-28 03:15:36 +0300

    Added links to header menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -12,31 +12,48 @@ const Header = () => {
         <div className='header'>
             <Logo itemClass={'itemClass'} />
             <div className='header-menu'>
-                <div>
-                    Home
-                </div>
-                <div className='header-menu__features'>
-                    Features
-                    <img className='header-menu__arrow'
-                        alt=''
-                        src="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' 
+                <a href='/'>
+                    <div>
+                        Home
+                    </div>
+                </a>
+                <a href='/'>
+                    <div className='header-menu__features'>
+
+                        Features
+                        <img className='header-menu__arrow'
+                            alt=''
+                            src="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' 
                         width='10' height='6'%3E%3Cpath d='M1 5l4-4 4 4' 
                         stroke='%23333333' fill='none'/%3E%3C/svg%3E"></img>
-                </div>
-                <div>Block</div>
-                <div>Shop</div>
-                <div>About</div>
-                <div className='header-menu__bold'>Contact</div>
+                    </div>
+                </a>
+                <a href='/'>
+                    <div>Block</div>
+                </a>
+                <a href='/'>
+                    <div>Shop</div>
+                </a>
+                <a href='/'>
+                    <div>About</div>
+                </a>
+                <a href='/'>
+                    <div className='header-menu__bold'>Contact</div>
+                </a>
                 <div className="round-container">
-                    <div className='round'>
-                        <img className='round-image' alt='' src={mask}></img>
-                    </div>
-                    <div className='round'>
-                        <img className='round-image' alt='' src={cart}></img>
-                    </div>
+                    <a href='/'>
+                        <div className='round'>
+                            <img className='round-image' alt='' src={mask}></img>
+                        </div>
+                    </a>
+                    <a href='/'>
+                        <div className='round'>
+                            <img className='round-image' alt='' src={cart}></img>
+                        </div>
+                    </a>
                 </div>
-            </div>
-        </div>
+            </div >
+        </div >
     )
 }
 
