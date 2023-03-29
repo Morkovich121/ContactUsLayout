3ed7032a2e28df98cb1004f7700b182c59674f34
DESKTOP-IOQP7MN\User <ilyasereda121@gmail.com>
2023-03-29 03:50:21 +0300

    Added fonts and done footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import Logo from '../Logo/Logo';
+import ContactInfo from '../ContactInfo/ContactInfo';
+
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <div className="footer-container">
+            <Logo itemClass='footer__logo' />
+            <div className='footer-info'>
+                <div className="footer-info__line"></div>
+                <div className="footer-info-text">
+                    <div className="gap20 footer-info_reach-us">
+                        <div className='footer-info__header'>Reach us</div>
+                        <ContactInfo />
+                    </div>
+                    <div className='footer-info-middle'>
+                        <div className="gap20 footer-info_company">
+                            <div className='footer-info__header'>Company</div>
+                            <div className='gap25 footer-contact-info'>
+                                <a href="/">About</a>
+                                <a href="/">Contact</a>
+                                <a href="/">Blogs</a>
+                            </div>
+                        </div>
+                        <div className="gap20 footer-info_legal">
+                            <div className='footer-info__header'>Legal</div>
+                            <div className='gap25 footer-contact-info'>
+                                <a href="/">Privacy Policy</a>
+                                <a href="/">Terms &amp; Services</a>
+                                <a href="/">Terms of Use</a>
+                                <a href="/">Refund Policy</a>
+                            </div>
+                        </div>
+                        <div className="gap20 footer-info_links">
+                            <div className='footer-info__header'>Quick Links</div>
+                            <div className='gap25 footer-contact-info'>
+                                <a href="/">Techlabz Keybox</a>
+                                <a href="/">Downloads</a>
+                                <a href="/">Forum</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='newsletter'>
+                        <div className="newsletter__header">
+                            Join Our Newsletter
+                        </div>
+                        <div className="newsletter-send">
+                            <input type='text' className='newsletter__input' placeholder='Your email address' />
+                            <button className='newsletter__button'>Subscribe</button>
+                        </div>
+                        <div className="newsletter__message">
+                            *  Will send you weekly updates for your better tool management.
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Footer from '../../components/Footer/Footer';
 import Form from '../../components/Form/Form';
 import Header from '../../components/Header/Header';
 
@@ -6,16 +7,20 @@ import './Contact.css';
 
 const Contact = () => {
     return (
-        <div className='contacts-container'>
-            <Header />
-            <div className="main-part">
-                <div className="main-part_contact-us">
-                    <span>Contact Us</span>
-                    <span>Any question or remarks? Just write us a message!</span>
+        <>
+            <div className='contacts-container'>
+                <Header />
+                <div className="main-part">
+                    <div className="main-part_contact-us">
+                        <span>Contact Us</span>
+                        <span>Any question or remarks? Just write us a message!</span>
+                    </div>
+                    <Form />
                 </div>
-                <Form />
             </div>
-        </div>
+            <Footer />
+        </>
+
     )
 }
 
